39c6891fa167005cfdd6facf8b90756d12a1823f
Fredrik Tuomas <fredrik.tuomas@gmail.com>
2013-10-12 17:18:41 +0200

    Limit number of photos to load in ui

--- src/main/java/se/bhg/photos/web/PhotosController.java
+++ src/main/java/se/bhg/photos/web/PhotosController.java
@@ -1,5 +1,9 @@
 package se.bhg.photos.web;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +30,18 @@ public class PhotosController {
     public String index(Model model) {
     	LOG.debug("Accessed photos controller");
     	Iterable<Photo> photos = photoService.getPhotos();
-    	model.addAttribute("photos", photos);
+    	List<Photo> full = new ArrayList<>(5000);
+    	List<Photo> subSet = new ArrayList<>(100);
+    	for(Photo p: photos) {
+    	    full.add(p);
+    	}
+    	Collections.shuffle(full);
+    	
+    	for(int n = 0; n < 100 && n < full.size(); n++) {
+    	    subSet.add(full.get(n));
+    	}
+    	
+    	model.addAttribute("photos", subSet);
         return "photos";
     }
 }
