e2678c69984ec121b146b3d335ea8ac186026812
Fredrik Tuomas <fredrik.tuomas@eniro.com>
2013-10-22 16:36:53 +0200

    Change to unicode

--- src/main/java/se/bhg/photos/config/ApplicationConfiguration.java
+++ src/main/java/se/bhg/photos/config/ApplicationConfiguration.java
@@ -118,7 +118,7 @@ public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
             ds.setDriverClassName("com.mysql.jdbc.Driver");
             ds.setUsername(username);
             ds.setPassword(password);
-            ds.setUrl("jdbc:mysql://localhost/bhg?characterEncoding=latin1");
+            ds.setUrl("jdbc:mysql://localhost/bhg?useUnicode=true&characterEncoding=UTF-8");
 
         } catch (Exception e) {
             //e.getMessage();
